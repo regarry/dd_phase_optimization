1bc932855cb4bc173b57a5124b31ecc4e2a2c126
Ryan Garry <regarry@ncsu.edu>
2025-10-17 18:36:01 -0400

    Add RunProfilerInference script and update beam_profiler and config settings
    - Introduced RunProfilerInference.py to orchestrate mask inference and beam profiling.
    - Modified beam_profiler.py to utilize lens_approach from config.
    - Updated config.yaml to change laser beam FWHC and lens approach parameters.

--- beam_profiler.py
+++ beam_profiler.py
@@ -115,9 +115,9 @@ def main():
     mask_tensor = torch.from_numpy(mask_np).type(torch.FloatTensor).to(device)
 
     config['Nimgs'] = 1
-    #lens_approach = config['lens_approach']
+    lens_approach = config['lens_approach']
     # to test 9f try running results with fourier lens
-    lens_approach = 'fourier_lens'  # temporarily force fourier lens for testing
+    #lens_approach = 'fourier_lens'  # temporarily force fourier lens for testing
     phys_layer = PhysicalLayer(config)
     phys_layer.eval()
     with torch.no_grad():
